3061d2ba5e0337f8e7a4e12cb9ab7c5124ee5c76
Patrick Lindsay <tlindsay@una.edu>
2013-03-10 18:28:11 -0500

    Created Project folder and begain coding!

---
+++ Project/header.php
@@ -0,0 +1,21 @@
+<?xml version = "1.0"  encoding = "utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+
+<html>
+	<head></head>
+	<body>
+		<div class="nav">
+			<ul>
+				<li class="nav" id="logo"><h3>UNA</h3><h5>Dept. Name</h5></li>
+				<li class="nav" id="home">Dashboard</li>
+				<li class="nav" id="class">Manage Classes</li>
+				<li class="nav" id="building">Manage Building</li>
+				<li class="nav" id="schedule">Manage Schedule</li>
+				<li class="nav" id="conflicts">Conflicts</li>
+				<li class="nav" id="help">Help</li>
+				<li class="nav" id="logout">Log Out</li>
+			</ul>
+		</div>
+		<div class="content">
+		
---
+++ Project/index.php
@@ -0,0 +1,5 @@
+<?php include("header.php"); ?>
+
+<h1>Hello, World!</h1>
+
+<?php include("footer.php"); ?>
