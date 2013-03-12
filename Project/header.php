e6a9d698e65835034ca85907a459938e941f8ece
Patrick Lindsay <tlindsay@una.edu>
2013-03-12 17:35:06 -0500

    Turned on Error Reporting

--- Project/header.php
+++ Project/header.php
@@ -2,6 +2,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 
+<?php error_reporting(E_ALL); ?>
+
 <html>
 	<head>
 		<link rel="stylesheet" href="css/main.css" type="text/css" />
