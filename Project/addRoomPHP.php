af6aa7a18d02b2c48ed62a34a30c5d4f856e5bbc
lylezpc <pzhao@una.edu>
2013-03-28 14:26:43 -0500

    Update addRoomPHP.php

--- Project/addRoomPHP.php
+++ Project/addRoomPHP.php
@@ -1,20 +1,25 @@
-<?php include("includes/header.php");
+<?php include("header.php");
 
 	// Get variables from input form
 	$type = $_POST['roomType'];
 	$size = $_POST['size'];
 	$name = $_POST['roomName'];
-	
+        
+        $link = mysql_connect (localhost,username, password);
+        if(!link){
+            die('cannot connect database'. mysql_error());
+        }
+        mysql_select_db (dbname);
+        mysql_query ("INSERT INTO tablename (roomType, size, roomName )
+        VALUES (\'$type\',\'$size\', \'$name\')
+        ");
+
 	// Print out contents accepted
 	echo "You have successfully added this course information to the database! <br>";
 	echo "Room Type: $type <br>";
 	echo "Room Size: $size <br>";
 	echo "Room Name: $name <br>";
 
+include("footer.php");
 
-
-
-
-
-include("includes/footer.php");
 ?>
