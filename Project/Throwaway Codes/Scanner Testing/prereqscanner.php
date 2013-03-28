a596dcd45acfa15d050ef59166e0f3be2b775382
Jared Cox <jmcox@una.edu>
2013-03-28 15:06:09 -0500

    Prereq Scanner
    Pushed prereq scanner to Project\Throwaway Codes\Scanner Testing

--- Project/Throwaway Codes/Scanner Testing/prereqscanner.php
+++ Project/Throwaway Codes/Scanner Testing/prereqscanner.php
@@ -2,26 +2,41 @@
 <body>
 
 <?php
+	
+	
 	//FLAGS
 		$firstCourseOnLineFlag = true;
+		$stillTesting = true;
 		
 	//FILE SETUP
-		$fileName = "prereq025";
+		$testName = "Tests/prereq";
+		$testNum = "1";
+while($stillTesting == true)
+{
+		$fileName = $testName . $testNum;
 		$log = $fileName . "Log";
 		
+
 		$readFile=fopen($fileName . ".txt","r") or die("Unable to open $fileName");
-		$logFile = fopen($log . ".txt", "w") or die("Unable to open $log");
+		$logFile = fopen($log . ".log", "w") or die("Unable to open $log");
+		
+		echo "<br> <br> <br>" . "Test File: $fileName" . "<br> <br> <br>";
+		
+	
 	
 	//VARIABLES	
 	$lineNumber = 0;
-	$printLine;			//line read in from file
+	$printLine = " ";	//line read in from file
+	$printLineLength = 255;
 	$printLineIndex = 0;	
 	$currentCourse = "";		//string		
 	$errorOnLine = false;
 	$listOfCourses = array();
 	$listOfCoursesIndex = 0;
 	$itemCount = 0;
-	
+	$errorInFile = false;
+	$firstLine = true;
+	$firstCourseNumber = 0;
 	
 	
 	while(!feof($readFile))
@@ -35,35 +50,48 @@
 		 $lineNumber++;
 		}while((strlen(trim($printLine)) == 0) and (!feof($readFile)));
 		
-		if(!feof($readFile))
-		{	
+		$printLine = $printLine . "\r";
+		$listOfCourses = array();
+		$listOfCoursesIndex = 0;
+			
 			$readLine = preg_split('/\s+/', $printLine);
-			//while(($itemCount < (count($readLine)-1)) and ($errorOnLine == false))
 			while(($printLineIndex < (strlen(trim($printLine)))) and ($errorOnLine == false))
 			{
 				echo "length of line is " . strlen($printLine) . "<br>";
-				echo "length of trimmed line is " . strlen(trim($printLine)) . "<br>";
+				echo "length of trimmed line is " . strlen(trim((string)$printLine)) . "<br>";
 				echo "line number $lineNumber and line index $printLineIndex" . "<br>";
 				
 				
 				if((count($readLine)) >= 3 and (count($readLine) <= 5))
 				{	
 					skipWhitespace($printLine, $printLineIndex); 
-					if(getCourse($printLine, $printLineIndex, $lineNumber, $firstCourseOnLineFlag, $currentCourse, $logFile) == false)
+					if(getCourse($printLine, $printLineIndex, $lineNumber, $currentCourse, $firstCourseOnLineFlag, $firstCourseNumber, $currentCourseNumber, $logFile) == false)
 					{
 						echo "getCourse returned false" . "<br>";
-						$errorOnLine = true;
+						$errorOnLine = true;  $errorInFile = true;
 						$itemCount++;
 					}
 					else
 					{
+						if(in_array($currentCourse, $listOfCourses) == true)
+						{
+							fputs($logFile, "Error on line $lineNumber.  Duplicate course found on line." . PHP_EOL);
+							$errorOnLine = true;  $errorInFile = true;
+						}
+						else
+						{
+							$listOfCourses[$listOfCoursesIndex] = $currentCourse;
+							$listOfCoursesIndex++;
+						}
+						if($firstCourseOnLineFlag == true)
+							$firstCourseOnLineFlag = false;
 						echo "getCourse returned true" . "<br>";
 						$itemCount++;
 						/*if($firstCourseOnLineFlag == true)
 						{
-							if $currentCourse is in $listOfCourses
+							if $currentCourse in COURSES database already has defined prerequisites
 							  {
-								fputs($logFile, "Error on line $lineNumber.  Course previously defined in file." . PHP_EOL);
+								fputs($logFile, "Error on line $lineNumber.  Course prerequisites already defined. All prerequisites for a course belong on the same line." . PHP_EOL);
 								errorOnLine = true;
 							  }
 							  else
@@ -81,15 +109,18 @@
 						}*/
 						if(($printLine[$printLineIndex] != " ") and ($printLine[$printLineIndex] != "\r"))
 						{
-							$errorOnLine = true;
-							fputs($logFile, "Error on line $lineNumber at index $printLineIndex.  Whitespace must separate elements on the line." . PHP_EOL);						
+							
+							
+								$errorOnLine = true;  $errorInFile = true;
+								fputs($logFile, "Error on line $lineNumber at index $printLineIndex.  Whitespace must separate elements on the line." . PHP_EOL);						
+							
 						}
 					}
 				}	
 				else
 				{
 					fputs($logFile, "Error on line $lineNumber at index $printLineIndex.  Courses in file must contain between 1 and 3 prerequisites." . PHP_EOL);
-					$errorOnLine = true;
+					$errorOnLine = true;  $errorInFile = true;
 				}
 			}
 			
@@ -101,13 +132,23 @@
 			else
 				echo $lineNumber . ": $printLine*" . "<br>";
 			
-		}
+		//}
+		$firstLine = false;
 	}
+	if($errorInFile == false)
+		fputs($logFile, "No errors detected." . PHP_EOL);
+		
 	fclose($readFile);	
 	fclose($logFile);
 	
+	$testNum += 1;
+	if($testNum > "45")
+		$stillTesting = false;
+}
+	
+	
 	/**********FUNCTIONS*********/
-	function getCourse($line, &$lineIndex, $lineNumber, &$firstCourseOnLineFlag, &$currentCourse, $logFile)
+	function getCourse($line, &$lineIndex, $lineNumber, &$currentCourse, $firstCourseOnLineFlag, &$firstCourseNumber, &$currentCourseNumber, $logFile)
 	{//function description
 	
 	 //function variables
@@ -131,7 +172,12 @@
 			{
 				fputs($logFile, "Error on line $lineNumber at index $lineIndex. Course letters must be between 2 and 4 characters." . PHP_EOL);
 				return false;
-			}
+			}/*
+			elseif($courseLetters not in Department Courses)
+			{
+				fputs($logFile, "Error on line $lineNumber at index $lineIndex.  Course letters is not a part of the department." . PHP_EOL);
+				return false;
+			*/
 			elseif(ctype_lower($line[$lineIndex]) == true)
 			{
 				fputs($logFile, "Error on line $lineNumber at index $lineIndex.  Files must contain ONLY uppercase letters." . PHP_EOL);
@@ -174,6 +220,19 @@
 								break;
 					}
 				}
+				//SPECIFIC TO PREREQSCANNER. REMOVE IF REUSED ELSEWHERE
+					if($firstCourseOnLineFlag == true)
+					{
+						$firstCourseNumber = $courseNumberInt;
+					}
+					elseif($courseNumberInt > $firstCourseNumber)
+					{
+						fputs($logFile, "Error on line $lineNumber at index $lineIndex.  Prerequisite is a higher level course than course requiring prerequisites." . PHP_EOL);
+						return false;
+					} 
+				///////////////////////////////////////////////////////
+				
+				
 				if(($courseNumberInt < 1) or ($courseNumberInt > 499))
 				{
 					fputs($logFile, "Error on line $lineNumber at index $lineIndex.  Course number exceeds boundaries. Must be between 001 and 499." . PHP_EOL);
@@ -181,6 +240,7 @@
 				}
 				else
 				{//COURSE NUMBER IS VALID
+				 //CHECK FOR LETTERS FOLLOWING COURSE NAME
 					$tempCourse = array_merge($courseLetters, $courseNumbers);
 					
 					while(ctype_upper($line[$lineIndex]))
@@ -194,10 +254,17 @@
 						fputs($logFile, "Error on line $lineNumber at index $lineIndex.  String of characters following course number is too long." . PHP_EOL);
 						return false;
 					}
+					elseif(($line[$lineIndex] !=  " ") and ($line[$lineIndex] != "\r"))
+					{
+						fputs($logFile, "Error on line $lineNumber at index $lineIndex.  Invalid character in string following course number." . PHP_EOL);
+						return false;
+					}
 					else
 					{
-						//if $tempCourse is not an empty array then
-						//$tempCourse = array_merge($tempCourse, $endCourseName);
+						if(!empty($endCourseName))
+						{
+							$tempCourse = array_merge($tempCourse, $endCourseName);
+						}
 					}
 				}
 			}
