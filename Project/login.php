03a9b05e554854e86608c3c5203940b215373cb8
lylezpc <pzhao@una.edu>
2013-04-01 01:54:10 -0500

    Create login.php

---
+++ Project/login.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="main.css" type="text/css" />
+        <title>Administrator Login</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>   
+        <div class="purpleBox">
+        <form id="login" name="login" method="post" action="chkunpw.php" onSubmit="">
+  	      <label for="Username">Username :</label>
+			      <input id="Username" name="Username" type="text" size="15"/> <br><br>
+		      <label for="Password">Password :</label>
+			      <input id="Password" name="Password" type="text" size="15" /> <br><br>
+		    <p>
+			    <input type="submit" name="submit" value="  submit  " />
+		    	<input type="reset" name="submit" value="  reset  "  />
+		    </p>
+        <p><img src="img/unaLogo.png" alt="UNA Logo" id="logo" /></p>
+        </form>
+        </div>
+    </body>
+</html>
