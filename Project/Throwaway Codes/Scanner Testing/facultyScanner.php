71215bac5e0f40af6aa7a18c70c5fbdd30aa4bc7
Michael Debs <madebs@una.edu>
2013-03-31 22:21:11 -0500

    Update on faculty scanner
    Added in a test file, just in case someone wants to run it

--- Project/Throwaway Codes/Scanner Testing/facultyScanner.php
+++ Project/Throwaway Codes/Scanner Testing/facultyScanner.php
@@ -9,7 +9,7 @@
         // put your code here
         
         $LineNumber=1;
-         $I=0;
+         
         $FacultyArray = array();
 		
 		
@@ -17,29 +17,30 @@
 		 
         while(!feof($InputFile))
         {
-           
+			$I=0;
             $Line = fgets($InputFile);
-            printf("Contents of line %d: %s <br>",$LineNumber,$Line);
-            //printf("BEFORE I= %d <br>",$I);
+			$Line = trim($Line);
+			
+			printf("Line = $Line <br>");
+			
             SkipWhitespace($Line, $I);
-            //printf("AFTER I= %d <br>",$I);
-			 $Value1=GetName($Line,$I,$LineNumber);
-			 SkipWhitespace($Line, $I);
+			
+			$Value1=GetName($Line,$I,$LineNumber);
+			SkipWhitespace($Line, $I);
 			 
-			 $Value2=GetYears($Line,$I,$LineNumber);
-			 SkipWhitespace($Line, $I);
+			$Value2=GetYears($Line,$I,$LineNumber);
+			SkipWhitespace($Line, $I);
 			 
-			 $Value3=GetEmail($Line,$I,$LineNumber);
-			 SkipWhitespace($Line, $I);
+			$Value3=GetEmail($Line,$I,$LineNumber);
+			SkipWhitespace($Line, $I);
 			 
-			 $Value4=GetMinHours($Line,$I ,$LineNumber);
+			$Value4=GetMinHours($Line,$I ,$LineNumber);
              
              
             
             
             //GetInfo($Line,$LineNumber,$FacultyArray,$I);
             $LineNumber= $LineNumber + 1;
-            $I= $I + 1;
           
 			printf("<br><br>");
            
@@ -55,14 +56,16 @@
         }//end function
         
         function SkipWhitespace($Line, &$LineIndex)
-	{
-	     while($Line[$LineIndex])== " " || ord($Line[$LineIndex])== "\t")
-		$LineIndex++;
-	}//end function
+		{
+			 while(ord($Line[$LineIndex]) == 32 || ord($Line[$LineIndex])== 9)
+			 {
+				$LineIndex++;
+			 }
+		}//end function
         
         function IsWhiteSpace ($Line, &$LineIndex, $LineNumber)
         {
-            if (ord($Line[$LineIndex])== 32 || ord($Line[$LineIndex]))
+            if (ord($Line[$LineIndex])== 32)
             {
                 $LineIndex++;
                 return true;
@@ -74,59 +77,61 @@
             }
         }//end function
         
-        function GetName($Line, &$Index, $LineNumber)
+        function GetName($Line, &$I, $LineNumber)
         {
-            $I=0;
             $LastName = "";
 			$FirstName = "";
-            
+  
             while($Line[$I] != ',' && strlen($LastName) <= 25)
             {
                 $LastName= $LastName .$Line[$I];
                 $I++;
             }
             $I++;
-            $Result= IsWhiteSpace($Line,$Index,$LineNumber);
+            if (IsWhiteSpace($Line,$I,$LineNumber) == true && strlen($LastName) <= 25)
+			{		
+				//Do i have to reset index I before the following loop?
+				while(ord($Line[$I]) != 32 && strlen($FirstName) <= (25 - strlen($LastName)) )
+				{
+					$FirstName = $FirstName .$Line[$I];
+					$I++;
+				}
+			}
             
-            //Do i have to reset index I before the following loop?
-            while(ord($Line[$I]) != 32 && strlen($FirstName) <= (25-  strlen($LastName)) )
-            {
-                $FirstName = $FirstName .$Line[$I];
-                $I++;
-            }
-            
-			printf("Name = $FirstName <br>");
+			printf("Name = $LastName, $FirstName <br>");
             //To truncate remaining characters
            // while(ord($Line[$I] !=  32))
              //   $I++;
-            //return true;
+            return true;
             
         }//end function
         
         function GetYears($Line,&$I,$LineNumber)
         {
-            $NumString = '';
-            while(ord($Line[$I]) >= 48 && ord($Line[$I])<=57) //is numeric
+            $NumString = "";
+            while(ord($Line[$I]) >= 48 && ord($Line[$I]) <= 57) //is numeric
+			{
                 $NumString= $NumString .$Line[$I];
+				$I++;
+			}
             
-            if(strlen($NumString)<0)
+            if(strlen($NumString) < 0)
             {
                 printf("Error on line %d, Years Of Service is less than zero.",$LineNumber);
                 return false;
             }
-            $ActualValue= GetOrdinalValue($NumString);
-            if($ActualValue > 102 )//'60'=102
+            if($NumString > 60)
             {
                 printf("Error on line %d, 60 is the maximum number of years of service",$LineNumber);
                 return false;
             }
-			printf("Years = $NumString");
+			printf("Years = $NumString <br>");
             return true;
         }//end function
         
         function GetEmail($Line,&$I, $LineNumber)
         {
-            $Email= '';
+            $Email= "";
             while($Line[$I] != '@' && strlen($Email)<=10)
             {
                 $Email= $Email .$Line[$I];
@@ -137,7 +142,7 @@
                 printf("Error on line %d, Email Address must be less than 10 characters.",$LineNumber);
                 return false;
             }
-            //Do I have to reset I ?
+			
             while(ord($Line[$I]) != 32)
             {
                 $Email= $Email .$Line[$I];
@@ -149,27 +154,28 @@
         
         function GetMinHours ($Line, &$I, $LineNumber)
         {
-            $NumString='';
+            $NumString= "";
             while (ord($Line[$I]) >=48 && ord($Line[$I]) <=57)
             {
-                    $NumString= $NumString .$Line[$I];
-                    $I++;
-            }      
-            $Value= GetOrdinalValue($NumString);
-            if($Value < 3)
+					$NumString= $NumString .$Line[$I];
+					$I++;
+            }
+            if($NumString < 3)
             {
                 printf("Error on line %d, Minimum number of hours to teach is 3.",$LineNumber);
                 return false;
             }
-            //I did not think that there was a maximum number of hours that faculty can teach.
-            else if ($Value > 12)
+            /*I did not think that there was a maximum number of hours that faculty can teach.
+            else if ($NumString > 12)
             {
                 printf("Error on line %d, maximum number of hours is 12.",$LineNumber);
                 return false;
-            }
+            }*/
+			
+			printf("Hours = $NumString <br>");
             return true;
 			
-			printf("Hours = $Value <br>");
+			
 			
         }//end function
         
