4f490513e3e5bc70cd31d1cb492d848d8d6783b5
Patrick Lindsay <tlindsay@una.edu>
2013-03-31 19:43:20 -0500

    Took out Doctype. It was throwing off PagodaBox

--- Project/includes/header.php
+++ Project/includes/header.php
@@ -1,7 +1,3 @@
-<?xml version = "1.0"  encoding = "utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-
 <?php include_once("global.php"); ?>
 <?php include_once("db.php"); ?>
 <?php error_reporting(E_ALL); ?>
