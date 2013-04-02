c31c74b6d893bba021f735681dca6cdbe852f1c9
Alla Salah <asalah@una.edu>
2013-04-02 10:48:25 -0500

    Updated Faculty Scanner
    Fixed some defects, changed code to satisfy coding standards, and
    included some documentation.

--- Project/Throwaway Codes/Scanner Testing/facultyScanner.php
+++ Project/Throwaway Codes/Scanner Testing/facultyScanner.php
@@ -8,41 +8,30 @@
         <?php
         // put your code here
         
-        $LineNumber=1;
-         
-        $FacultyArray = array();
-		
-		
-         $InputFile = fopen ("FACULTY18.txt","r") or die ("Unable to open file");
+        $lineNumber=1; 
+        $facultyArray = array();
+        $inputFile = fopen ("FACULTY18.txt","r") or die ("Unable to open file");
 		 
-        while(!feof($InputFile))
+        while(!feof($inputFile))
         {
-			$I=0;
-            $Line = fgets($InputFile);
-			$Line = trim($Line);
-			
-			printf("Line = $Line <br>");
-			
-            SkipWhitespace($Line, $I);
-			
-			$Value1=GetName($Line,$I,$LineNumber);
-			SkipWhitespace($Line, $I);
-			 
-			$Value2=GetYears($Line,$I,$LineNumber);
-			SkipWhitespace($Line, $I);
-			 
-			$Value3=GetEmail($Line,$I,$LineNumber);
-			SkipWhitespace($Line, $I);
-			 
-			$Value4=GetMinHours($Line,$I ,$LineNumber);
-             
-             
-            
-            
-            //GetInfo($Line,$LineNumber,$FacultyArray,$I);
-            $LineNumber= $LineNumber + 1;
-          
-			printf("<br><br>");
+	    $i=0;
+            $line = fgets($inputFile);
+            $line = trim($line);		
+	    printf("Line = $line <br>");		
+            skipWhiteSpace($line, $i);		
+	    
+            $value1=getName($line,$i,$lineNumber);
+	    skipWhiteSpace($line, $i);	 
+	    
+            $value2=getYears($line,$i,$lineNumber);
+	    skipWhiteSpace($line, $i);	 
+	    
+            $value3=getEmail($line,$i,$lineNumber);
+	    skipWhiteSpace($line, $i);			 
+	    
+            $value4=getMinHours($line,$i ,$lineNumber);
+            $lineNumber= $lineNumber + 1;
+            printf("<br><br>");
            
         }
         
@@ -50,143 +39,228 @@
         //Functions
         //*************************************
         
-        function GetInfo($Line, $LineNumber, $RoomInfo)
-        {
-            
-        }//end function
         
-        function SkipWhitespace($Line, &$LineIndex)
+         /*
+         **-----------------------------------------------------------------------------------  
+         * Purpose:
+         * 
+         * Input:
+         * 
+         * Output:
+         **-----------------------------------------------------------------------------------  
+         */
+        function skipWhiteSpace($line, &$lineIndex)
 		{
-			 while(ord($Line[$LineIndex]) == 32 || ord($Line[$LineIndex])== 9)
+			 while(ord($line[$lineIndex]) == 32 || ord($line[$lineIndex])== 9)
 			 {
-				$LineIndex++;
+				$lineIndex++;
 			 }
 		}//end function
         
-        function IsWhiteSpace ($Line, &$LineIndex, $LineNumber)
+         /*
+         **-----------------------------------------------------------------------------------  
+         * Purpose:
+         * 
+         * Input:
+         * 
+         * Output:
+         **-----------------------------------------------------------------------------------  
+         */        
+        function isWhiteSpace ($line, &$lineIndex, $lineNumber)
         {
-            if (ord($Line[$LineIndex])== 32)
+            if (ord($line[$lineIndex])== 32)
             {
-                $LineIndex++;
+                $lineIndex++;
                 return true;
             }
             else
             {
-                printf("Error on line %d : Exprected White Space.<br>",$LineNumber);
+                printf("Error on line %d : Exprected White Space.<br>",$lineNumber);
                 return false;
             }
         }//end function
         
-        function GetName($Line, &$I, $LineNumber)
+         /*
+         **-----------------------------------------------------------------------------------  
+         * Purpose:
+         * 
+         * Input:
+         * 
+         * Output:
+         **-----------------------------------------------------------------------------------  
+         */
+        function getName($line, &$i, $lineNumber)
         {
-            $LastName = "";
-			$FirstName = "";
+            $lastName = "";
+			$firstName = "";
   
-            while($Line[$I] != ',' && strlen($LastName) <= 25)
+            while($line[$i] != ',' && strlen($lastName) <= 25)
             {
-                $LastName= $LastName .$Line[$I];
-                $I++;
+                $lastName= $lastName .$line[$i];
+                $i++;
             }
-            $I++;
-            if (IsWhiteSpace($Line,$I,$LineNumber) == true && strlen($LastName) <= 25)
+            $i++;
+            if (isWhiteSpace($line,$i,$lineNumber) == true && strlen($lastName) <= 25)
 			{		
 				//Do i have to reset index I before the following loop?
-				while(ord($Line[$I]) != 32 && strlen($FirstName) <= (25 - strlen($LastName)) )
+				while(ord($line[$i]) != 32 && strlen($firstName) <= (25 - strlen($lastName)) )
 				{
-					$FirstName = $FirstName .$Line[$I];
-					$I++;
+					$firstName = $firstName .$line[$i];
+					$i++;
 				}
 			}
             
-			printf("Name = $LastName, $FirstName <br>");
+			printf("Name = $lastName, $firstName <br>");
             //To truncate remaining characters
-           // while(ord($Line[$I] !=  32))
-             //   $I++;
+           // while(ord($line[$i] !=  32))
+             //   $i++;
             return true;
             
         }//end function
         
-        function GetYears($Line,&$I,$LineNumber)
+         /*
+         **----------------------------------------------------------------------------------- 
+         * Purpose: Parses the input line to get the years of service.
+         * 
+         * Input: The input line read from file called $line
+         *        The index in the array to start reading from called $i
+         *        The line number in the input file for error messages called $line number
+         * 
+         * Output: Error message if invalid data is found or if years of service does not meet
+         *         the specified guidelines.
+         * Return : True if the years of service data is correct. Returns false if not. 
+         **-----------------------------------------------------------------------------------  
+         */
+        function getYears($line,&$i,$lineNumber)
         {
-            $NumString = "";
-            while(ord($Line[$I]) >= 48 && ord($Line[$I]) <= 57) //is numeric
+            $numString = "";
+            while(ord($line[$i]) >= 48 && ord($line[$i]) <= 57) //is numeric
 			{
-                $NumString= $NumString .$Line[$I];
-				$I++;
+                                $numString= $numString .$line[$i];
+				$i++;
 			}
             
-            if(strlen($NumString) < 0)
+            if(strlen($numString) < 0)
             {
-                printf("Error on line %d, Years Of Service is less than zero.",$LineNumber);
+                printf("Error on line %d, Years Of Service is less than zero.",$lineNumber);
                 return false;
             }
-            if($NumString > 60)
+            if($numString > 60)
             {
-                printf("Error on line %d, 60 is the maximum number of years of service",$LineNumber);
+                printf("Error on line %d, 60 is the maximum number of years of service",$lineNumber);
                 return false;
             }
-			printf("Years = $NumString <br>");
+			printf("Years = $numString <br>");
             return true;
         }//end function
         
-        function GetEmail($Line,&$I, $LineNumber)
+         /*
+         *----------------------------------------------------------------------------------- 
+         * Purpose: Parses the line string to get and validate email address
+         *          of the faculty member. If email address is valid it is inserted into the 
+         *          faculty array to check for duplicates.
+         * Input: The input line read from file called $line
+         *        The index in the array to start reading from called $i
+         *        The line number in the input file for error messages called $line number
+         * Output: An error message if the email information is not valid according to the 
+         *         specified requirements/guidelines.
+         * Return: True if the email address is valid. Otherwise returns false.
+         *----------------------------------------------------------------------------------- 
+         */
+        function getEmail($line,&$i, $lineNumber)
         {
-            $Email= "";
-            while($Line[$I] != '@' && strlen($Email)<=10)
+            //for($k=0; $k < strlen($line);$k++)
+               // printf("k= %d %s--%d <br>",$k,$line[$k],ord($line[$k]));
+            //printf("size of line= %d <br>",strlen($line) );
+            $email= "";
+            
+            //Gets the email address except for the extension
+            while($line[$i] != '@' && strlen($email)<=10)
             {
-                $Email= $Email .$Line[$I];
-                $I++;
+                $email= $email .$line[$i];
+                $i++;
             }
-            if(strlen($Email)> 10)
+            if(strlen($email)> 10)
             {
-                printf("Error on line %d, Email Address must be less than 10 characters.",$LineNumber);
+                printf("Error on line %d, Email Address must be less than 10 characters.",$lineNumber);
                 return false;
             }
 			
-            while(ord($Line[$I]) != 32)
+            //Gets the email extension; for example, @UNA.EDU
+            while(ord($line[$i]) != 32 && ord($line[$i]) != 9)
             {
-                $Email= $Email .$Line[$I];
-                $I++;
+                $email= $email .$line[$i];
+                $i++;
             }
 			
-			printf("Email = $Email <br>");
+			printf("Email = $email <br>");
         }//end function
         
-        function GetMinHours ($Line, &$I, $LineNumber)
+         /*
+         **----------------------------------------------------------------------------------- 
+         * Purpose: Parses the line string to get the minimum hours teaching.
+         * 
+         * Input: The input line read from file called $line
+         *        The index in the array to start reading from called $i
+         *        The line number in the input file for error messages called $line number
+         * 
+         * Output: Error message if the minimum hours is invalid. 
+         * Return: True if the minimum hours teaching is within the specified guidelines. Returns
+         *         false if the hours is incorrect.
+         **----------------------------------------------------------------------------------- 
+         */
+        function getMinHours ($line, &$i, $lineNumber)
         {
-            $NumString= "";
-            while (ord($Line[$I]) >=48 && ord($Line[$I]) <=57)
+            $numString= "";
+            //for($k=0; $k < strlen($line);$k++)
+               // printf("k= %d %s--%d <br>",$k,$line[$k],ord($line[$k]));
+            //printf("size of line= %d <br>",strlen($line) );
+            while (ord($line[$i]) >=48 && ord($line[$i]) <=57)
             {
-					$NumString= $NumString .$Line[$I];
-					$I++;
+					$numString= $numString .$line[$i];
+					$i++;
+                                        //Exit condition
+                                        if($i == strlen($line))
+                                        {
+                                            break;
+                                        }
             }
-            if($NumString < 3)
+            if($numString < 3)
             {
-                printf("Error on line %d, Minimum number of hours to teach is 3.",$LineNumber);
+                printf("Error on line %d, Minimum number of hours to teach is 3.",$lineNumber);
                 return false;
             }
             /*I did not think that there was a maximum number of hours that faculty can teach.
-            else if ($NumString > 12)
+            else if ($numString > 12)
             {
-                printf("Error on line %d, maximum number of hours is 12.",$LineNumber);
+                printf("Error on line %d, maximum number of hours is 12.",$lineNumber);
                 return false;
             }*/
 			
-			printf("Hours = $NumString <br>");
+			printf("Hours = $numString <br>");
             return true;
 			
 			
 			
         }//end function
         
-        function GetOrdinalValue ($word)
+         /*
+         **----------------------------------------------------------------------------------- 
+         * Purpose:
+         * 
+         * Input:
+         * 
+         * Output:
+         **----------------------------------------------------------------------------------- 
+         */
+        function getOrdinalValue ($word)
           {
              //Varaiable declarations
-              $OrdinalSum=0;
-              for($I=0; $I < strlen($word);$I++)
-                    $OrdinalSum= $OrdinalSum + ord($word[$I]);
+              $ordinalSum=0;
+              for($i=0; $i < strlen($word);$i++)
+                    $ordinalSum= $ordinalSum + ord($word[$i]);
               
-              return $OrdinalSum;
+              return $ordinalSum;
           }//end function
          
 
