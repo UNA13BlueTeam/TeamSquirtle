b97497fd25b90b06948693554ad4fdb0c783e7d6
Patrick Lindsay <tlindsay@una.edu>
2013-04-07 14:39:38 -0500

    More styling

--- Project/addClass.php
+++ Project/addClass.php
@@ -6,27 +6,45 @@
 <div class="purpleBox">
 	<form class="inputForm" id="scheduleForm" name="scheduleForm" method="post" action="doAddClass.php" onSubmit="return InputCheck(this)">
 		<input type="hidden" name="flag" value="form">
-		<div class="row"><label for="course">Course</label>
-			<input id="course" name="course" type="text" maxlength="6"/></div> <br>
-		<div class="row"><label for="dsection">No. of Day Sections</label>
-			<input id="dsection" name="dsection" type="text" maxlength="6"/></div> <br>
-		<div class="row"><label for="nsection">No. of Night Sections</label>
-			<input id="nsection" name="nsection" type="text" maxlength="6"/></div> <br>
-		<div class="row"><label for="isection">No. of Internet Sections</label>
-			<input id="isection" name="isection" type="text" maxlength="6"/></div> <br>
-		<div class="row"><label for="classSize">Class Size</label>
-			<input id="classSize" name="classSize" type="text" maxlength="6"/></div> <br>
-		<div class="row"><label for="roomType">Class Type</label>
+		<div class="row">
+			<label for="course">Course</label>
+			<input id="course" name="course" type="text" maxlength="6"/>
+		</div> <br> <hr>
+		<div class="row">
+			<label for="dsection">No. of Day Sections</label>
+			<input id="dsection" name="dsection" type="text" maxlength="6"/>
+		</div> <br> <hr>
+		<div class="row">
+			<label for="nsection">No. of Night Sections</label>
+			<input id="nsection" name="nsection" type="text" maxlength="6"/>
+		</div> <br> <hr>
+		<div class="row">
+			<label for="isection">No. of Internet Sections</label>
+			<input id="isection" name="isection" type="text" maxlength="6"/>
+		</div> <br> <hr>
+		<div class="row">
+			<label for="classSize">Class Size</label>
+			<input id="classSize" name="classSize" type="text" maxlength="6"/>
+		</div> <br> <hr>
+		<div class="row">
+			<label for="roomType">Class Type</label>
 			<select name="roomType">
 				<option value="C">Class</option>
 				<option value="L">Lab</option>
-			</select></div> <br>
-		<div class="row"><label for="hours">Credit Hours</label>
-			<input id="hours" name="hours" type="text" maxlength="2"/></div> <br>
-		<div class="row"><label for="prereq">Prerequisites</label>
-			<input id="prereq" name="prereq" type="text" maxlength="15"/></div> <br>
-		<div class="row"><label for="conflict">Conflicts</label>
-			<input id="conflict" name="conflict" type="text" maxlength="15"/></div> <br>
+			</select>
+		</div> <br> <hr>
+		<div class="row">
+			<label for="hours">Credit Hours</label>
+			<input id="hours" name="hours" type="text" maxlength="2"/>
+		</div> <br> <hr>
+		<div class="row">
+			<label for="prereq">Prerequisites</label>
+			<input id="prereq" name="prereq" type="text" maxlength="15"/>
+		</div> <br> <hr>
+		<div class="row">
+			<label for="conflict">Conflicts</label>
+			<input id="conflict" name="conflict" type="text" maxlength="15"/>
+		</div> <br> <hr>
 			<input id="button" type="submit" name="submit" value="Submit" />
 			<input id="button" type="reset" name="reset" value="Reset"  />
 	</form>
