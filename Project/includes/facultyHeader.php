c1b7eae4994d86bb28c453bb51c04b500b6a45da
lylezpc <pzhao@una.edu>
2013-04-07 14:22:34 -0500

    Create facultyHeader.php

---
+++ Project/includes/facultyHeader.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    
+  <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" href="css/main.css" type="text/css" />
+		<link rel="icon" type="image/png" href="img/squirtle.png">
+	</head>
+	<body>
+	<div class="nav">
+		<nav>
+			<a href="index.php">UNA-Dept. Name</a>
+			<a href="index.php" id="home">Home</a>
+			<a href="viewschedule.php" id="timeSlots">View Schedules</a> 
+			<a href="Pickcourses.php" id="building">Pick Courses</a>
+			<a href="help.php" id="help">Help</a>
+			<a href="logout.php" id="logout">Log Out</a>
+			<img src="img/unaLogo.png" alt="UNA Logo" id="logo" />
+		</nav>
+	</div>
+		<div class="content">
