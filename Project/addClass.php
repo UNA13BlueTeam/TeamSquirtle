4b60fcf7ce5bb11608d22d2d88318f98b36cb985
Patrick Lindsay <tlindsay@una.edu>
2013-04-07 14:28:53 -0500

    Fixed some styling issues

--- Project/addClass.php
+++ Project/addClass.php
@@ -6,31 +6,29 @@
 <div class="purpleBox">
 	<form class="inputForm" id="scheduleForm" name="scheduleForm" method="post" action="doAddClass.php" onSubmit="return InputCheck(this)">
 		<input type="hidden" name="flag" value="form">
-		<label for="course">Course</label>
-			<input id="course" name="course" type="text" size="15" maxlength="6"/> <br>
-		<label for="dsection">No. of Day Sections</label>
-			<input id="dsection" name="dsection" type="text" size="15" maxlength="6"/> <br>
-		<label for="nsection">No. of Night Sections</label>
-			<input id="nsection" name="nsection" type="text" size="15" maxlength="6"/> <br>
-		<label for="isection">No. of Internet Sections</label>
-			<input id="isection" name="isection" type="text" size="15" maxlength="6"/> <br>
-		<label for="classSize">Class Size</label>
-			<input id="classSize" name="classSize" type="text" size="15" maxlength="6"/> <br>
-		<label for="roomType">Class Type</label>
+		<div class="row"><label for="course">Course</label>
+			<input id="course" name="course" type="text" maxlength="6"/></div> <br>
+		<div class="row"><label for="dsection">No. of Day Sections</label>
+			<input id="dsection" name="dsection" type="text" maxlength="6"/></div> <br>
+		<div class="row"><label for="nsection">No. of Night Sections</label>
+			<input id="nsection" name="nsection" type="text" maxlength="6"/></div> <br>
+		<div class="row"><label for="isection">No. of Internet Sections</label>
+			<input id="isection" name="isection" type="text" maxlength="6"/></div> <br>
+		<div class="row"><label for="classSize">Class Size</label>
+			<input id="classSize" name="classSize" type="text" maxlength="6"/></div> <br>
+		<div class="row"><label for="roomType">Class Type</label>
 			<select name="roomType">
 				<option value="C">Class</option>
 				<option value="L">Lab</option>
-			</select> <br>
-		<label for="hours">Credit Hours</label>
-			<input id="hours" name="hours" type="text" size="15" maxlength="2"/> <br>
-		<label for="prereq">Prerequisites</label>
-			<input id="prereq" name="prereq" type="text" size="15" maxlength="15"/> <br>
-		<label for="conflict">Conflicts</label>
-			<input id="conflict" name="conflict" type="text" size="15" maxlength="15"/> <br>
-		<p>
-			<input type="submit" name="submit" value="  submit  " />
-			<input type="reset" name="submit" value="  reset  "  />
-		</p>
+			</select></div> <br>
+		<div class="row"><label for="hours">Credit Hours</label>
+			<input id="hours" name="hours" type="text" maxlength="2"/></div> <br>
+		<div class="row"><label for="prereq">Prerequisites</label>
+			<input id="prereq" name="prereq" type="text" maxlength="15"/></div> <br>
+		<div class="row"><label for="conflict">Conflicts</label>
+			<input id="conflict" name="conflict" type="text" maxlength="15"/></div> <br>
+			<input id="button" type="submit" name="submit" value="Submit" />
+			<input id="button" type="reset" name="reset" value="Reset"  />
 	</form>
 </div>
 
@@ -41,8 +39,8 @@
 			<input type="file" name="classFile"> <br /><br />
 		<label for="prereqFile">Prequisite File:</label>
 			<input type="file" name="prereqFile"><br><br>
-		<input type="submit" name="submit" value="  submit file " />
-		<input type="reset" name="submit" value="  reset  "  />
+		<input type="submit" name="submit" value="Submit File" />
+		<input type="reset" name="submit" value="Reset"  />
 	</form>
 </div>
 
