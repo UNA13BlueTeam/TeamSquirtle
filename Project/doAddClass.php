4aa4b1f2471c39a146e3b40d605b7070b0aa4cad
Jared Cox <jmcox@una.edu>
2013-04-09 16:36:16 -0500

    made the same change
    Apparently it didn't take? Idk.

--- Project/doAddClass.php
+++ Project/doAddClass.php
@@ -509,7 +509,7 @@ include("includes/footer.php");
 						echo("Error on line $lineNumber at index $lineIndex.  String of characters following course number is too long." . PHP_EOL);
 						return false;
 					}
-					elseif(($line[$lineIndex] !=  " ") and ($line[$lineIndex] != "\r") and ($line[$lineIndex] != "\r"))
+					elseif(($line[$lineIndex] !=  " ") and ($line[$lineIndex] != "\r") and ($line[$lineIndex] != "\t"))
 					{//only whitespace or carriage return can immediately follow a course on line
 						echo("Error on line $lineNumber at index $lineIndex.  Invalid character in string following course number." . PHP_EOL);
 						return false;
