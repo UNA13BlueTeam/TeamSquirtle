a0ed9be98c6eafcf3f3c18c68bdf2b5420084271
lylezpc <pzhao@una.edu>
2013-04-09 20:08:24 -0500

    Update chkunpw.php

--- Project/chkunpw.php
+++ Project/chkunpw.php
@@ -3,24 +3,32 @@
     <title>Check username and password</title>
 </head>
 <body>
-    <?php include("includes/db.php"); 
-    $link = mysqli_connect ($host, $user, $pass, $db);
-    if(!$link){
+    <?php include_once("includes/global.php"); ?>
+    <?php include("includes/db.php"); ?>
+    <?php 
+    		global $host, $user, $pass, $db, $port;
+			$test = mysqli_connect($host, $user, $pass, $db, $port);
+    
+    if(!$test){
       echo 'fail to connect database';
     }
     $password = trim($_POST['Password']);
     $username = $_POST['Username'];
-    $admin ="Select * from useradmin where username='$username' and userpass='$password'";
-    $faculty = "Select * from userfaculty where username='$username' and userpass='$password'";
-    $adminquery = mysqli_query( $link, $admin );
-    $facultyquery = mysqli_query( $link, $faculty );
-    if(mysqli_num_rows( $adminquery )){
-        echo 'admin login';
-        header('adminhome.php');
+    $admin ="Select * from users where username='$username' and password='$password'";
+    $faculty = "Select * from userfaculty where username='$username' and password='$password'";
+    $facultyquery = mysqli_query( $test, $faculty );
+    $queryResult = mysqli_query( $test, $admin );
+    if($queryResult===false){
+        printf("error: %s\n", mysqli_error($test));
     }
-    elseif(mysqli_num_rows( $facultyquery )){
+    $result = mysqli_num_rows( $queryResult );
+    if($result){
+        echo 'admin login';
+        header("Location: index1.php");
+    }
+    elseif(mysqli_num_rows($facultyquery )){
         echo 'faculty login';
-        header('facultyhome.php');
+        header("Location: facultyHome.php");
     }
     else{
         echo 'login failure';
