15c5b3702cdfb5007b0b2eb443d0e69b3bcbbf93
Patrick Lindsay <tlindsay@una.edu>
2013-04-15 23:48:29 -0500

    Began implementing firstLogOn handling

--- Project/index.php
+++ Project/index.php
@@ -5,6 +5,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="css/main.css" type="text/css" />
+        <link rel="icon" type="image/png" href="img/squirtle.png">
         <title>Login</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     </head>
@@ -12,20 +13,28 @@
     <?php 
         if(!isset($_POST['flag']))
         {
-            $_SESSION['loggedIn'] = false;
+            // $_SESSION['loggedIn'] = false;
             include("login.php");
         }else
         {
-            
             $password = $_POST['password'];
             $username = $_POST['username'];
             global $host, $user, $pass, $db, $port;
             $link = mysqli_connect($host, $user, $pass, $db, $port);
-            $query = "SELECT permission FROM users WHERE username='$username' AND password='$password'";
+            $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
             $result = mysqli_query($link, $query);
             $temp = array();
-            $temp = mysqli_fetch_row($result);
-            $auth = $temp[0];
+            $temp = mysqli_fetch_assoc($result);
+            $auth = $temp['permission'];
+            $setup = $temp['firstLogOn'];
+            echo($setup);
+            // while($row = mysqli_fetch_row($result))
+            // {
+            //     foreach($row as &$value)
+            //     {
+            //         array_push($_SESSION, $value);
+            //     }
+            // }
 
             if($auth==1)
             {
@@ -33,18 +42,30 @@
                 $_SESSION['permission'] = "admin";
                 $permission = $_SESSION['permission'];
                 $_SESSION['username'] = $username;
-                include("adminHome.php");
+                if($setup==1)
+                {
+                    header("Location: setup.php");
+                }else
+                {
+                    header("Location: adminHome.php");
+                }
             }elseif($auth==2)
             {
                 $_SESSION['loggedIn'] = true;
                 $_SESSION['permission'] = "faculty";
                 $permission = $_SESSION['permission'];
                 $_SESSION['username'] = $username;
-                include("facultyHome.php");
+                if($setup==1)
+                {
+                    header("Location: setup.php");
+                }else
+                {
+                    header("Location: facultyHome.php");
+                }
             }else
             {
                 // User fails authentication. Output login page.
-                $_SESSION['loggedIn'] = false;
+                // $_SESSION['loggedIn'] = false;
                 $_SESSION['username'] = NULL;
                 $_SESSION['permission'] = NULL;
                 include("login.php");
