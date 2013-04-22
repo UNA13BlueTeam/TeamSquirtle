ccf8605fc26c551cb793bb54cb78ace1772f9aa2
Patrick Lindsay <tlindsay@una.edu>
2013-04-21 19:14:58 -0500

    A little reformatting on Header to make it more useable

--- Project/includes/header.php
+++ Project/includes/header.php
@@ -20,13 +20,14 @@
 				echo('
 					<div class="nav">
 						<nav>
-							<a href="adminHome.php">UNA '.$deptAbbrev.'</a>
-							<a href="adminHome.php" id="home">Home</a>
-							<a href="manageTimeSlots.php" id="timeSlots">Manage Class Times</a> 
-							<a href="manageRooms.php" id="building">Manage Rooms</a>
-							<a href="manageClass.php" id="classes">Manage Classes</a>
-							<a href="manageFaculty.php" id="faculty">Manage Faculty</a>
-							<a href="help.php" id="help">Help</a>
+							<!-- <a href="adminHome.php">UNA '.$deptAbbrev.'</a> -->
+							<a href="adminHome.php" id="home">Home</a>&nbsp;|&nbsp;
+							<a href="manageTimeSlots.php" id="timeSlots">Manage Class Times</a> &nbsp;|&nbsp;
+							<a href="manageRooms.php" id="building">Manage Rooms</a>&nbsp;|&nbsp;
+							<a href="manageClass.php" id="classes">Manage Classes</a>&nbsp;|&nbsp;
+							<a href="manageFaculty.php" id="faculty">Manage Faculty</a>&nbsp;|&nbsp;
+							<a href="help.php" id="help">Help</a>&nbsp;|&nbsp;
+							<a href="adminActions.php" id="help">Options</a>&nbsp;|&nbsp;
 							<a href="logout.php" id="logout">Log Out</a>
 							<img src="img/unaLogo.png" alt="UNA Logo" id="logo" />
 						</nav>
@@ -36,12 +37,12 @@
 				echo('
 					<div class="nav">
 						<nav>
-							<a href="facultyHome.php">UNA '.$deptAbbrev.'</a>
-							<a href="facultyHome.php" id="home">Home</a>
-							<a href="viewschedule.php" id="timeSlots">View Schedules</a> 
-							<a href="Pickcourses.php" id="building">Pick Courses</a>
-							<a href="facultyHelp.php" id="help">Help</a>
-							<a href="logout.php" id="logout">Log Out</a>
+							<!-- <a href="facultyHome.php">UNA '.$deptAbbrev.'</a> -->
+							<a href="facultyHome.php" id="home">Home</a>&nbsp;|&nbsp;
+							<a href="viewschedule.php" id="timeSlots">View Schedules</a> &nbsp;|&nbsp;
+							<a href="Pickcourses.php" id="building">Pick Courses</a>&nbsp;|&nbsp;
+							<a href="facultyHelp.php" id="help">Help</a>&nbsp;|&nbsp;
+							<a href="logout.php" id="logout">Log Out</a>&nbsp;|&nbsp;
 							<img src="img/unaLogo.png" alt="UNA Logo" id="logo" />
 						</nav>
 				');
@@ -50,7 +51,7 @@
 				echo('
 					<div class="nav">
 						<nav>
-							<a href="adminHome.php">UNA </a>
+							<!-- <a href="adminHome.php">UNA </a> -->
 							<a href="adminHome.php" id="home">Home</a>
 							<a href="manageTimeSlots.php" id="timeSlots">Manage Class Times</a> 
 							<a href="manageRooms.php" id="building">Manage Rooms</a>
