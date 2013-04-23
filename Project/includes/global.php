7f8db69d4bb7119bb452517f690754a4fcd2958a
Patrick Lindsay <tlindsay@una.edu>
2013-04-22 20:51:53 -0500

    Implementing file generators

--- Project/includes/global.php
+++ Project/includes/global.php
@@ -20,7 +20,6 @@
 			$infoResults = mysqli_query($link, $infoQuery);
 			$userInfo = mysqli_fetch_assoc($infoResults);
 			$deptName 	= $userInfo['deptName'];
-			$deptAbbrev = "CSIS";
 		}else
 		{
 			// echo("LoggedIn = false <br>");
@@ -30,38 +29,133 @@
 		// echo("loggedIn unset. <br>");
 	}
 
-	function clearUsers(){
+################################################################
+// Table Clearing Functions
+################################################################
+
+	function clearFaculty()
+	{
+		global $link;
 		$query = "TRUNCATE TABLE faculty; DELETE * FROM users WHERE permission != 1;";
 		mysqli_query($link, $query);
 	}
 
-	function clearClasses(){
+	function clearClasses()
+	{
+		global $link;
 		$query = "TRUNCATE TABLE courses";
 		mysqli_query($link, $query);
 	}
 
-	function clearClassTimes(){
+	function clearClassTimes()
+	{
+		global $link;
 		$query = "TRUNCATE TABLE timeSlots";
 		mysqli_query($link, $query);
 	}
 
-	function clearRooms(){
+	function clearRooms()
+	{
+		global $link;
 		$query = "TRUNCATE TABLE rooms";
 		mysqli_query($link, $query);
 	}
 
-	function clearPrereqs(){
+	function clearPrereqs()
+	{
+		global $link;
 		$query = "TRUNCATE TABLE prereqs";
 		mysqli_query($link, $query);
 	}
 
-	function clearPrefs(){
+	function clearPrefs()
+	{
+		global $link;
 		$query = "TRUNCATE TABLE preferences";
 		mysqli_query($link, $query);
 	}
 
-	function clearSchedule(){
+	function clearSchedule()
+	{
+		global $link;
 		$query = "TRUNCATE TABLE scheduledCourses";
 		mysqli_query($link, $query);
 	}
+
+################################################################
+// File Generators
+################################################################	
+
+	function generateFaculty()
+	{
+		global $link;
+		$outFile = fopen("generatedFiles/faculty.txt", "w");
+		$query = "SELECT * FROM faculty";
+		$results = mysqli_query($link, $query);
+		while($row = mysqli_fetch_assoc($results))
+		{
+			$output = $row['facultyName']." ".$row['yos']." ".$row['email']."@UNA.EDU ".$row['minHours']."\r\n";
+		}
+	}
+
+	function generateClasses()
+	{
+		global $link;
+		$outFile = fopen("generatedFiles/courses.txt", "w");
+		$query = "SELECT * FROM courses";
+		$results = mysqli_query($link, $query);
+		while($row = mysqli_fetch_assoc($results))
+		{
+			$output = $row['courseName']." ".$row['dsection']." ".$row['nsection']." ".$row['isection']." ".$row['classSize']." ".$row['roomType']." ".$row['hours']."\r\n";
+		}
+	}
+
+	function generateClassTimes()
+	{
+		global $link;
+		$outFile = fopen("generatedFiles/classTimes.txt", "w");
+		$query = "SELECT * FROM timeSlots";
+		$results = mysqli_query($link, $query);
+		while($row = mysqli_fetch_assoc($results))
+		{
+			$output = $row['minutes']." ".$row['daysOfWeek']."/".$row['timesOfDay']."\r\n";
+		}
+	}
+
+	function generateRooms()
+	{
+		global $link;
+		$outFile = fopen("generatedFiles/rooms.txt", "w");
+		$query = "SELECT * FROM rooms";
+		$results = mysqli_query($link, $query);
+		while($row = mysqli_fetch_assoc($results))
+		{
+			$output = $row['roomType']." ".$row['size']." ".$row['roomName']."\r\n";
+		}
+	}
+
+	function generatePrereqs()
+	{
+		global $link;
+		$outFile = fopen("generatedFiles/prereqs.txt", "w");
+		$query = "SELECT * FROM prereqs";
+		$results = mysqli_query($link, $query);
+		while($row = mysqli_fetch_assoc($results))
+		{
+			$output = $row['course']." ".$row['prereq1']." ".$row['prereq2']." ".$row['prereq3']."\r\n";
+		}
+	}
+
+	function generateConflicts()
+	{
+		global $link;
+		$outFile = fopen("generatedFiles/conflicts.txt", "w");
+		$query = "SELECT * FROM conflicts";
+		$results = mysqli_query($link, $query);
+		while($row = mysqli_fetch_assoc($results))
+		{
+			$output = $row['course']." ".$row['times']."\r\n";
+		}
+	}
+
 ?>
